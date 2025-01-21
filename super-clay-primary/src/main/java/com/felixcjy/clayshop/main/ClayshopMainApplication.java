0f3e7d069171238dbdfd9311436571326b738724
Felix Cai <3049564188@qq.com>
2025-01-21 10:57:00 +0800

    init: 增加主要子项目

---
+++ super-clay-primary/src/main/java/com/felixcjy/clayshop/main/ClayshopMainApplication.java
@@ -0,0 +1,13 @@
+package com.felixcjy.clayshop.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClayshopMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClayshopMainApplication.class, args);
+    }
+
+}
