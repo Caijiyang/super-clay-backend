ed0fba0322ede4fafc6b9834981862ed5b481a73
Felix Cai <3049564188@qq.com>
2025-01-21 17:00:56 +0800

    init: 新增system模块

--- super-clay-primary/src/main/java/com/felixcjy/superclay/primary/ClayshopMainApplication.java
+++ super-clay-primary/src/main/java/com/felixcjy/superclay/primary/ClayshopMainApplication.java
@@ -1,4 +1,4 @@
-package com.felixcjy.clayshop.main;
+package com.felixcjy.superclay.primary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
